e0d0fae6ea48a840fe0a55a9fc44c069e912c0f4
Island GiftCards <info@islandgiftcards.net>
2025-04-01 11:23:33 -0500

    fix: export createUserWithEmailAndPassword from firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,19 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { createUserWithEmailAndPassword, signOut } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getAuth, createUserWithEmailAndPassword, signOut } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDZnMtl_BXxZPj0thle_TwlFFYozZFfOMo",
   authDomain: "igc-redeem-merchant-portal.firebaseapp.com",
   projectId: "igc-redeem-merchant-portal",
-  storageBucket: "igc-redeem-merchant-portal.firebasestorage.app",
+  storageBucket: "igc-redeem-merchant-portal.appspot.com",
   messagingSenderId: "559704332284",
   appId: "1:559704332284:web:ca1d3ec020d1dbe7800479",
   measurementId: "G-6VQCVS356K"
 };
 
 const app = initializeApp(firebaseConfig);
+getAnalytics(app);
+
 export const auth = getAuth(app);
+export { createUserWithEmailAndPassword, signOut };
